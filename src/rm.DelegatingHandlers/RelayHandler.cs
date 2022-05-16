a9f0fc64d76f5a8016d8d0fc546c24146788d2b9
hippy <hippy2981@gmail.com>
2022-05-15 22:16:19 -0700

    dh: Add Relay handler

---
+++ src/rm.DelegatingHandlers/RelayHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Simply relays request, just for fun.
+	/// </summary>
+	public class RelayHandler : DelegatingHandler
+	{
+		/// <inheritdoc cref="RelayHandler" />
+		public RelayHandler()
+		{ }
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+}
