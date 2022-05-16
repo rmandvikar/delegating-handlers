1656e3620de7ee823b3c74c682d3fb34d2865c6b
hippy <hippy2981@gmail.com>
2022-05-15 22:19:14 -0700

    dh: Add Retry handler

---
+++ src/rm.DelegatingHandlers/misc/HttpStatusCodeExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+
+namespace rm.DelegatingHandlers
+{
+	public static class HttpStatusCodeExtensions
+	{
+		public static bool Is1xx(this HttpStatusCode statusCode)
+		{
+			return GetStatusCodeClassDigit(statusCode) == 1;
+		}
+
+		public static bool Is2xx(this HttpStatusCode statusCode)
+		{
+			return GetStatusCodeClassDigit(statusCode) == 2;
+		}
+
+		public static bool Is3xx(this HttpStatusCode statusCode)
+		{
+			return GetStatusCodeClassDigit(statusCode) == 3;
+		}
+
+		public static bool Is4xx(this HttpStatusCode statusCode)
+		{
+			return GetStatusCodeClassDigit(statusCode) == 4;
+		}
+
+		public static bool Is5xx(this HttpStatusCode statusCode)
+		{
+			return GetStatusCodeClassDigit(statusCode) == 5;
+		}
+
+		// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+		private static int GetStatusCodeClassDigit(HttpStatusCode statusCode)
+		{
+			return (int)statusCode / 100;
+		}
+
+		/// <summary>
+		/// Returns true if status code is a client error status code (4xx).
+		/// </summary>
+		public static bool IsClientErrorStatusCode(this HttpStatusCode statusCode)
+		{
+			return statusCode.Is4xx();
+		}
+
+		/// <summary>
+		/// Returns true if status code is a server error status code (5xx).
+		/// </summary>
+		public static bool IsServerErrorStatusCode(this HttpStatusCode statusCode)
+		{
+			return statusCode.Is5xx();
+		}
+
+		/// <summary>
+		/// Returns true if status code is an error status code (4xx, 5xx).
+		/// </summary>
+		public static bool IsErrorStatusCode(this HttpStatusCode statusCode)
+		{
+			return statusCode.Is4xx() || statusCode.Is5xx();
+		}
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/misc/HttpMessageInvokerFactory.cs
@@ -0,0 +1,36 @@
+namespace rm.DelegatingHandlersTest
+{
+	public static class HttpMessageInvokerFactory
+	{
+		public static HttpMessageInvoker Create(params DelegatingHandler[] handlers)
+		{
+			return Create(null!, handlers);
+		}
+
+		public static HttpMessageInvoker Create(HttpMessageHandler innerHandler, params DelegatingHandler[] handlers)
+		{
+			if (handlers == null || !handlers.Any())
+			{
+				throw new ArgumentNullException(nameof(handlers));
+			}
+			if (handlers.Any(x => x == null))
+			{
+				throw new ArgumentNullException(nameof(handlers), "At least one of the handlers is null.");
+			}
+
+			var first = handlers[0];
+
+			Array.Reverse(handlers);
+			var current = innerHandler;
+			foreach (var next in handlers)
+			{
+				if (current != null)
+				{
+					next.InnerHandler = current;
+				}
+				current = next;
+			}
+			return new HttpMessageInvoker(first);
+		}
+	}
+}
