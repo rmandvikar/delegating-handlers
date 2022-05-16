2dfae8c4cd7ff8e554bbfec3b2b42bcd9e43b913
hippy <hippy2981@gmail.com>
2022-05-15 22:12:00 -0700

    dh: Add Procrastinating handlers

---
+++ src/rm.DelegatingHandlers/ProcrastinatingHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Causes delay to induce http timeouts.
+	/// </summary>
+	public class ProcrastinatingHandler : DelegatingHandler
+	{
+		private readonly IProcrastinatingHandlerSettings procrastinatingHandlerSettings;
+
+		/// <inheritdoc cref="ProcrastinatingHandler" />
+		public ProcrastinatingHandler(
+			IProcrastinatingHandlerSettings procrastinatingHandlerSettings)
+		{
+			this.procrastinatingHandlerSettings = procrastinatingHandlerSettings
+				?? throw new ArgumentNullException(nameof(procrastinatingHandlerSettings));
+		}
+
+		protected override async Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			await Task.Delay(procrastinatingHandlerSettings.DelayInMilliseconds, cancellationToken)
+				.ConfigureAwait(false);
+
+			return await base.SendAsync(request, cancellationToken)
+				.ConfigureAwait(false);
+		}
+	}
+
+	public interface IProcrastinatingHandlerSettings
+	{
+		int DelayInMilliseconds { get; }
+	}
+
+	public record class ProcrastinatingHandlerSettings : IProcrastinatingHandlerSettings
+	{
+		public int DelayInMilliseconds { get; init; }
+	}
+}
---
+++ src/rm.DelegatingHandlers/ProcrastinatingWithProbabilityHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using rm.FeatureToggle;
+using rm.Random2;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Causes delay with a probability to induce http timeouts.
+	/// </summary>
+	public class ProcrastinatingWithProbabilityHandler : DelegatingHandler
+	{
+		private readonly IProcrastinatingWithProbabilityHandlerSettings procrastinatingWithProbabilityHandlerSettings;
+
+		private readonly IProbability probability = new Probability(rng);
+		private static readonly Random rng = RandomFactory.GetThreadStaticRandom();
+
+		/// <inheritdoc cref="ProcrastinatingWithProbabilityHandler" />
+		public ProcrastinatingWithProbabilityHandler(IProcrastinatingWithProbabilityHandlerSettings procrastinatingWithProbabilityHandlerSettings)
+		{
+			this.procrastinatingWithProbabilityHandlerSettings = procrastinatingWithProbabilityHandlerSettings
+				?? throw new ArgumentNullException(nameof(procrastinatingWithProbabilityHandlerSettings));
+		}
+
+		protected override async Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			if (probability.IsTrue(procrastinatingWithProbabilityHandlerSettings.ProbabilityPercentage))
+			{
+				await Task.Delay(procrastinatingWithProbabilityHandlerSettings.DelayInMilliseconds, cancellationToken)
+					.ConfigureAwait(false);
+			}
+
+			return await base.SendAsync(request, cancellationToken)
+				.ConfigureAwait(false);
+		}
+	}
+
+	public interface IProcrastinatingWithProbabilityHandlerSettings
+	{
+		double ProbabilityPercentage { get; }
+		int DelayInMilliseconds { get; }
+	}
+
+	public record class ProcrastinatingWithProbabilityHandlerSettings : IProcrastinatingWithProbabilityHandlerSettings
+	{
+		public double ProbabilityPercentage { get; init; }
+		public int DelayInMilliseconds { get; init; }
+	}
+}
