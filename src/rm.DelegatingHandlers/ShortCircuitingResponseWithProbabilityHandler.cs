927ea0bba65c56f64e5c6208f7bf024491decbf1
hippy <hippy2981@gmail.com>
2022-05-15 22:09:45 -0700

    dh: Add ShortCircuiting handlers

---
+++ src/rm.DelegatingHandlers/ShortCircuitingResponseWithProbabilityHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using rm.FeatureToggle;
+using rm.Random2;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Causes http response for given statusCode, content with a probability.
+	/// </summary>
+	public class ShortCircuitingResponseWithProbabilityHandler : DelegatingHandler
+	{
+		private readonly IShortCircuitingResponseWithProbabilityHandlerSettings shortCircuitingResponseWithProbabilityHandlerSettings;
+
+		private readonly IProbability probability = new Probability(rng);
+		private static readonly Random rng = RandomFactory.GetThreadStaticRandom();
+
+		/// <inheritdoc cref="ShortCircuitingResponseWithProbabilityHandler" />
+		public ShortCircuitingResponseWithProbabilityHandler(
+			IShortCircuitingResponseWithProbabilityHandlerSettings shortCircuitingResponseWithProbabilityHandlerSettings)
+		{
+			this.shortCircuitingResponseWithProbabilityHandlerSettings = shortCircuitingResponseWithProbabilityHandlerSettings
+				?? throw new ArgumentNullException(nameof(shortCircuitingResponseWithProbabilityHandlerSettings));
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			if (probability.IsTrue(shortCircuitingResponseWithProbabilityHandlerSettings.ProbabilityPercentage))
+			{
+				return Task.FromResult(
+					new HttpResponseMessage
+					{
+						StatusCode = shortCircuitingResponseWithProbabilityHandlerSettings.StatusCode,
+						ReasonPhrase = $"{nameof(ShortCircuitingResponseWithProbabilityHandler)} says hello!",
+						Content =
+							new StringContent(
+								shortCircuitingResponseWithProbabilityHandlerSettings.Content,
+								Encoding.UTF8,
+								MediaTypeNames.Application.Json),
+					});
+			}
+
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+
+	public interface IShortCircuitingResponseWithProbabilityHandlerSettings
+	{
+		double ProbabilityPercentage { get; }
+		HttpStatusCode StatusCode { get; }
+		string Content { get; }
+	}
+
+	public record class ShortCircuitingResponseWithProbabilityHandlerSettings : IShortCircuitingResponseWithProbabilityHandlerSettings
+	{
+		public double ProbabilityPercentage { get; init; }
+		public HttpStatusCode StatusCode { get; init; }
+		public string Content { get; init; }
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/ShortCircuitingResponseHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ShortCircuitingResponseHandlerTests
+	{
+		[Test]
+		public async Task ShortCircuits()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var statusCode = (HttpStatusCode)42;
+			var content = fixture.Create<string>();
+			var shortCircuitingResponseHandler = new ShortCircuitingResponseHandler(
+				new ShortCircuitingResponseHandlerSettings
+				{
+					StatusCode = statusCode,
+					Content = content,
+				});
+			shortCircuitingResponseHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(shortCircuitingResponseHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(statusCode, response.StatusCode);
+			Assert.AreEqual(content, await response.Content.ReadAsStringAsync());
+			Assert.AreEqual($"{nameof(ShortCircuitingResponseHandler)} says hello!", response.ReasonPhrase);
+		}
+	}
+}
