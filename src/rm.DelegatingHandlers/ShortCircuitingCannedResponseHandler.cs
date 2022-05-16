566dc9634e90d26464248f4c4f3fa52f9735d5de
hippy <hippy2981@gmail.com>
2022-05-15 22:09:45 -0700

    dh: Add ShortCircuiting handlers

---
+++ src/rm.DelegatingHandlers/ShortCircuitingCannedResponseHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Short-circuits with a canned http response.
+	/// </summary>
+	/// <remarks>
+	/// Canned response should not be disposed if used with a retry handler
+	/// as it's meant for multiuse. If so, consider using <see cref="ShortCircuitingResponseHandler"/>
+	/// instead.
+	/// </remarks>
+	public class ShortCircuitingCannedResponseHandler : DelegatingHandler
+	{
+		private readonly HttpResponseMessage response;
+
+		/// <inheritdoc cref="ShortCircuitingCannedResponseHandler" />
+		public ShortCircuitingCannedResponseHandler(
+			HttpResponseMessage response)
+		{
+			this.response = response
+				?? throw new ArgumentNullException(nameof(response));
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			return Task.FromResult(response);
+		}
+	}
+}
---
+++ src/rm.DelegatingHandlers/misc/MediaTypeNames.cs
@@ -0,0 +1,11 @@
+﻿namespace rm.DelegatingHandlers
+{
+	// ref: https://github.com/dotnet/corefx/blob/master/src/System.Net.Mail/src/System/Net/Mime/MediaTypeNames.cs
+	public static class MediaTypeNames
+	{
+		public static class Application
+		{
+			public const string Json = "application/json";
+		}
+	}
+}
