ddfd2818508c9ae3a7c5fc9e6e87b89ec1e1a42d
hippy <hippy2981@gmail.com>
2022-05-15 22:16:19 -0700

    dh: Add Relay handler

---
+++ tests/rm.DelegatingHandlersTest/RelayHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class RelayHandlerTests
+	{
+		[Test]
+		public async Task Relays()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var relayHandler = new RelayHandler();
+			relayHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(relayHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+		}
+	}
+}
