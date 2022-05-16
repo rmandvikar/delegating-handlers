713e649c3ec18921f74dbf0bcc328123b6c6c2db
hippy <hippy2981@gmail.com>
2022-05-15 22:12:00 -0700

    dh: Add Procrastinating handlers

---
+++ tests/rm.DelegatingHandlersTest/ProcrastinatingHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ProcrastinatingHandlerTests
+	{
+		[Test]
+		public async Task Procrastinates()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var delayInMilliseconds = 25;
+			var procrastinatingHandler = new ProcrastinatingHandler(
+				new ProcrastinatingHandlerSettings
+				{
+					DelayInMilliseconds = delayInMilliseconds,
+				});
+			procrastinatingHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(procrastinatingHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			var stopwatch = Stopwatch.StartNew();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			stopwatch.Stop();
+			Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+			Assert.GreaterOrEqual(stopwatch.ElapsedMilliseconds, delayInMilliseconds);
+		}
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/ProcrastinatingWithProbabilityHandlerTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ProcrastinatingWithProbabilityHandlerTests
+	{
+		[Test]
+		public async Task Procrastinates()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var delayInMilliseconds = 25;
+			var procrastinatingWithProbabilityHandler = new ProcrastinatingWithProbabilityHandler(
+				new ProcrastinatingWithProbabilityHandlerSettings
+				{
+					ProbabilityPercentage = 100d,
+					DelayInMilliseconds = delayInMilliseconds,
+				});
+			procrastinatingWithProbabilityHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(procrastinatingWithProbabilityHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			var stopwatch = Stopwatch.StartNew();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			stopwatch.Stop();
+			Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+			Assert.GreaterOrEqual(stopwatch.ElapsedMilliseconds, delayInMilliseconds);
+		}
+
+		[Test]
+		public async Task Does_Not_Procrastinate()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var delayInMilliseconds = 1000;
+			var procrastinatingWithProbabilityHandler = new ProcrastinatingWithProbabilityHandler(
+				new ProcrastinatingWithProbabilityHandlerSettings
+				{
+					ProbabilityPercentage = 0d,
+					DelayInMilliseconds = delayInMilliseconds,
+				});
+			procrastinatingWithProbabilityHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(procrastinatingWithProbabilityHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			var stopwatch = Stopwatch.StartNew();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			stopwatch.Stop();
+			Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+			Assert.Less(stopwatch.ElapsedMilliseconds, delayInMilliseconds);
+		}
+	}
+}
