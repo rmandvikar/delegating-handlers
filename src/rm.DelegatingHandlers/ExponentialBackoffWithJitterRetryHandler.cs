18015c0dd38ad5109726e9c9e5455f122564871c
hippy <hippy2981@gmail.com>
2022-05-15 22:19:14 -0700

    dh: Add Retry handler

---
+++ src/rm.DelegatingHandlers/ExponentialBackoffWithJitterRetryHandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Retries on certain conditions with exponential backoff jitter (DecorrelatedJitterBackoffV2).
+	/// <para></para>
+	/// Retry conditions:
+	///   HttpRequestException, 5xx.
+	/// </summary>
+	/// <remarks>
+	/// <see href="https://github.com/App-vNext/Polly/wiki/Retry-with-jitter">source</see>
+	/// </remarks>
+	public class ExponentialBackoffWithJitterRetryHandler : DelegatingHandler
+	{
+		private readonly IAsyncPolicy<HttpResponseMessage> retryPolicy;
+
+		/// <inheritdoc cref="ExponentialBackoffWithJitterRetryHandler" />
+		public ExponentialBackoffWithJitterRetryHandler(
+			IRetrySettings retrySettings)
+		{
+			_ = retrySettings
+				?? throw new ArgumentNullException(nameof(retrySettings));
+
+			var sleepDurationsWithJitter = Backoff.DecorrelatedJitterBackoffV2(
+				medianFirstRetryDelay: TimeSpan.FromMilliseconds(retrySettings.RetryDelayInMilliseconds),
+				retryCount: retrySettings.RetryCount);
+
+			// note: response can't be null
+			// ref: https://github.com/dotnet/runtime/issues/19925#issuecomment-272664671
+			retryPolicy = Policy
+				.Handle<HttpRequestException>()
+				.OrResult<HttpResponseMessage>(response => response.Is5xx())
+				.WaitAndRetryAsync(
+					sleepDurations: sleepDurationsWithJitter,
+					onRetry: (responseResult, delay, retryAttempt, context) =>
+					{
+						// note: response can be null in case of handled exception
+						responseResult.Result?.Dispose();
+						context[ContextKey.RetryAttempt] = retryAttempt;
+					});
+		}
+
+		protected async override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			return await retryPolicy.ExecuteAsync(
+				action: async (context, ct) =>
+				{
+					if (context.TryGetValue(ContextKey.RetryAttempt, out var retryAttempt))
+					{
+						request.Properties[RequestProperties.PollyRetryAttempt] = retryAttempt;
+					}
+					return await base.SendAsync(request, ct)
+						.ConfigureAwait(false);
+				},
+				context: new Context(),
+				cancellationToken: cancellationToken)
+					.ConfigureAwait(false);
+		}
+	}
+
+	public interface IRetrySettings
+	{
+		int RetryCount { get; }
+		int RetryDelayInMilliseconds { get; }
+	}
+
+	public record class RetrySettings : IRetrySettings
+	{
+		public int RetryCount { get; init; }
+		public int RetryDelayInMilliseconds { get; init; }
+	}
+
+	internal static class ContextKey
+	{
+		internal const string RetryAttempt = nameof(RetryAttempt);
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/ExponentialBackoffWithJitterRetryHandlerTests.cs
@@ -0,0 +1,176 @@
+﻿using System.Net;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ExponentialBackoffWithJitterRetryHandlerTests
+	{
+		[Test]
+		public async Task Retries_On_5xx()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var statusCode = (HttpStatusCode)542;
+			var content = fixture.Create<string>();
+			var shortCircuitingResponseHandler = new ShortCircuitingResponseHandler(
+				new ShortCircuitingResponseHandlerSettings
+				{
+					StatusCode = statusCode,
+					Content = content,
+				});
+			var retryAttempt = -1;
+			var delegateHandler = new DelegateHandler(
+				(request, ct) =>
+				{
+					retryAttempt++;
+					return Task.CompletedTask;
+				});
+			var retryHandler = new ExponentialBackoffWithJitterRetryHandler(
+				new RetrySettings
+				{
+					RetryCount = 1,
+					RetryDelayInMilliseconds = 0,
+				});
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				fixture.Create<HttpMessageHandler>(), retryHandler, delegateHandler, shortCircuitingResponseHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(1, retryAttempt);
+		}
+
+		[Test]
+		public void Retries_On_HttpRequestException()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var throwingHandler = new ThrowingHandler(new HttpRequestException());
+			var retryAttempt = -1;
+			var delegateHandler = new DelegateHandler(
+				(request, ct) =>
+				{
+					retryAttempt++;
+					return Task.CompletedTask;
+				});
+			var retryHandler = new ExponentialBackoffWithJitterRetryHandler(
+				new RetrySettings
+				{
+					RetryCount = 1,
+					RetryDelayInMilliseconds = 0,
+				});
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				retryHandler, delegateHandler, throwingHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			Assert.ThrowsAsync<HttpRequestException>(async () =>
+			{
+				using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			});
+
+			Assert.AreEqual(1, retryAttempt);
+		}
+
+		[Test]
+		public void Does_Not_Retries_On_TaskCanceledException()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var throwingHandler = new ThrowingHandler(new TaskCanceledException());
+			var retryAttempt = -1;
+			var delegateHandler = new DelegateHandler(
+				(request, ct) =>
+				{
+					retryAttempt++;
+					return Task.CompletedTask;
+				});
+			var retryHandler = new ExponentialBackoffWithJitterRetryHandler(
+				new RetrySettings
+				{
+					RetryCount = 1,
+					RetryDelayInMilliseconds = 0,
+				});
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				fixture.Create<HttpMessageHandler>(), retryHandler, delegateHandler, throwingHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			Assert.ThrowsAsync<TaskCanceledException>(async () =>
+			{
+				using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			});
+
+			Assert.AreEqual(0, retryAttempt);
+		}
+
+		[Test]
+		public async Task When_0_Retries_PollyRetryAttempt_Property_Is_Not_Present()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var statusCode = (HttpStatusCode)542;
+			var content = fixture.Create<string>();
+			var shortCircuitingResponseHandler = new ShortCircuitingResponseHandler(
+				new ShortCircuitingResponseHandlerSettings
+				{
+					StatusCode = statusCode,
+					Content = content,
+				});
+			var retryHandler = new ExponentialBackoffWithJitterRetryHandler(
+				new RetrySettings
+				{
+					RetryCount = 0,
+					RetryDelayInMilliseconds = 0,
+				});
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				fixture.Create<HttpMessageHandler>(), retryHandler, shortCircuitingResponseHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+			Assert.IsFalse(requestMessage.Properties.ContainsKey(RequestProperties.PollyRetryAttempt));
+#pragma warning restore CS0618 // Type or member is obsolete
+		}
+
+		[Test]
+		[TestCase(1)]
+		[TestCase(2)]
+		public async Task When_N_Retries_PollyRetryAttempt_Property_Is_Present(int retryCount)
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var statusCode = (HttpStatusCode)542;
+			var content = fixture.Create<string>();
+			var shortCircuitingResponseHandler = new ShortCircuitingResponseHandler(
+				new ShortCircuitingResponseHandlerSettings
+				{
+					StatusCode = statusCode,
+					Content = content,
+				});
+			var retryHandler = new ExponentialBackoffWithJitterRetryHandler(
+				new RetrySettings
+				{
+					RetryCount = retryCount,
+					RetryDelayInMilliseconds = 0,
+				});
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				retryHandler, shortCircuitingResponseHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+			Assert.AreEqual(retryCount, requestMessage.Properties[RequestProperties.PollyRetryAttempt]);
+#pragma warning restore CS0618 // Type or member is obsolete
+		}
+	}
+}
