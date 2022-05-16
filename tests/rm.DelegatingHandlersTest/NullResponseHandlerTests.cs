a742ba2e11d3727d79ac8aab591ae4085ba44a43
hippy <hippy2981@gmail.com>
2022-05-15 22:15:19 -0700

    dh: Add NullReponse handler

---
+++ tests/rm.DelegatingHandlersTest/NullResponseHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class NullResponseHandlerTests
+	{
+		[Test]
+		public async Task Nulls()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var nullResponseHandler = new NullResponseHandler();
+			nullResponseHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(nullResponseHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.IsNull(response);
+		}
+	}
+}
