a8c991367c43f04e19d2d42a4c6b9e0766d0b18d
hippy <hippy2981@gmail.com>
2022-05-15 21:13:23 -0700

    dh: Add Throwing handlers

---
+++ tests/rm.DelegatingHandlersTest/ThrowingHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ThrowingHandlerTests
+	{
+		[Test]
+		public void Throws()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var throwingHandler = new ThrowingHandler(new TurnDownForWhatException());
+			throwingHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(throwingHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			var ex = Assert.ThrowsAsync<TurnDownForWhatException>(async () =>
+			{
+				using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			});
+		}
+	}
+}
