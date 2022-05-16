639c0da1575a7b802ceae50fbc1f38b321855e24
hippy <hippy2981@gmail.com>
2022-05-16 00:32:20 -0700

    WIP dh: Teach Retry handler to react to retry-after header

--- src/rm.DelegatingHandlers/ExponentialBackoffWithJitterRetryHandler.cs
+++ src/rm.DelegatingHandlers/ExponentialBackoffWithJitterRetryHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Polly;
 using Polly.Contrib.WaitAndRetry;
+using rm.Extensions;
 
 namespace rm.DelegatingHandlers
 {
@@ -11,7 +13,13 @@ namespace rm.DelegatingHandlers
 	/// Retries on certain conditions with exponential backoff jitter (DecorrelatedJitterBackoffV2).
 	/// <para></para>
 	/// Retry conditions:
-	///   HttpRequestException, 5xx.
+	///   HttpRequestException, 5xx, 429 (see retry-after header below).
+	///   <br/>
+	/// retry-after header:
+	///   <br/>
+	///   For 503: retry honoring header if present, else retry as usual.
+	///   <br/>
+	///   For 429: retry only if header present, and conditions are met.
 	/// </summary>
 	/// <remarks>
 	/// <see href="https://github.com/App-vNext/Polly/wiki/Retry-with-jitter">source</see>
@@ -19,30 +27,90 @@ namespace rm.DelegatingHandlers
 	public class ExponentialBackoffWithJitterRetryHandler : DelegatingHandler
 	{
 		private readonly IAsyncPolicy<HttpResponseMessage> retryPolicy;
+		private readonly IRetrySettings retrySettings;
 
 		/// <inheritdoc cref="ExponentialBackoffWithJitterRetryHandler" />
 		public ExponentialBackoffWithJitterRetryHandler(
 			IRetrySettings retrySettings)
 		{
-			_ = retrySettings
+			this.retrySettings = retrySettings
 				?? throw new ArgumentNullException(nameof(retrySettings));
 
 			var sleepDurationsWithJitter = Backoff.DecorrelatedJitterBackoffV2(
 				medianFirstRetryDelay: TimeSpan.FromMilliseconds(retrySettings.RetryDelayInMilliseconds),
 				retryCount: retrySettings.RetryCount);
 
+			var maxRetryDelayInMilliseconds = Backoff.ExponentialBackoff(
+				initialDelay: TimeSpan.FromMilliseconds(retrySettings.RetryDelayInMilliseconds),
+				retryCount: retrySettings.RetryCount)
+				.Last().TotalMilliseconds;
+
 			// note: response can't be null
 			// ref: https://github.com/dotnet/runtime/issues/19925#issuecomment-272664671
 			retryPolicy = Policy
 				.Handle<HttpRequestException>()
 				.Or<TimeoutExpiredException>()
-				.OrResult<HttpResponseMessage>(response => response.Is5xx())
+				.OrResult<HttpResponseMessage>(response =>
+				{
+					// handle retry-after response header if present
+					var retryAfterValue = (string)null;
+					var hasRetryAfter = response.Headers.TryGetValues(ResponseHeaders.RetryAfter, out var retryAfterValues)
+						&& (retryAfterValue = retryAfterValues.OneOrDefault()) != null;
+					var isRetryAfterWithinRetryWindow = (bool?)null;
+					if (hasRetryAfter)
+					{
+						isRetryAfterWithinRetryWindow =
+							(double.TryParse(retryAfterValue, out double retryAfterSeconds)
+								// ignore network latency
+								&& TimeSpan.FromSeconds(retryAfterSeconds).TotalMilliseconds <= maxRetryDelayInMilliseconds)
+							||
+							(DateTimeOffset.TryParse(retryAfterValue, out DateTimeOffset retryAfterDate)
+								&& (retryAfterDate - DateTimeOffset.UtcNow).TotalMilliseconds <= maxRetryDelayInMilliseconds);
+					}
+					var statusCode = (int)response.StatusCode;
+					return
+						(response.Is5xx() && statusCode != 503)
+							|| (statusCode == 503 && (!hasRetryAfter || (hasRetryAfter && isRetryAfterWithinRetryWindow.Value)))
+							// retry 429 only if retry-after response header present
+							|| (statusCode == 429 && hasRetryAfter && isRetryAfterWithinRetryWindow.Value);
+				})
 				.WaitAndRetryAsync(
-					sleepDurations: sleepDurationsWithJitter,
+					retryCount: retrySettings.RetryCount,
+					sleepDurationProvider: (retryAttempt, responseResult, context) =>
+					{
+						// handle retry-after response header if present
+						var retryAfter = TimeSpan.Zero;
+						var isRetryAfterValid = false;
+						// note: response can be null in case of handled exception
+						var response = responseResult.Result;
+						if (response != null)
+						{
+							var retryAfterValue = (string)null;
+							var statusCode = (int)response.StatusCode;
+							isRetryAfterValid =
+								(statusCode == 503
+									|| statusCode == 429)
+								&& response.Headers.TryGetValues(ResponseHeaders.RetryAfter, out var retryAfterValues)
+									&& (retryAfterValue = retryAfterValues.OneOrDefault()) != null
+								&&
+									((double.TryParse(retryAfterValue, out double retryAfterSeconds)
+										// ignore network latency
+										&& (retryAfter = TimeSpan.FromSeconds(retryAfterSeconds)).TotalSeconds > 0)
+									||
+									(DateTimeOffset.TryParse(retryAfterValue, out DateTimeOffset retryAfterDate)
+										&& (retryAfter = retryAfterDate - DateTimeOffset.UtcNow).TotalSeconds > 0));
+						}
+						var retryIndex = retryAttempt - 1;
+						var sleepDurationWithJitter = sleepDurationsWithJitter;
+						return isRetryAfterValid
+							// wait for higher of the two values
+							? MathHelper.Max(retryAfter, sleepDurationWithJitter)
+							: sleepDurationWithJitter;
+					},
 					onRetry: (responseResult, delay, retryAttempt, context) =>
 					{
-						// note: response can be null in case of handled exception
 						responseResult.Result?.Dispose();
+
 						context[ContextKey.RetryAttempt] = retryAttempt;
 					});
 		}
@@ -58,6 +126,7 @@ namespace rm.DelegatingHandlers
 					{
 						request.Properties[RequestProperties.PollyRetryAttempt] = retryAttempt;
 					}
+
 					return await base.SendAsync(request, ct)
 						.ConfigureAwait(false);
 				},
---
+++ src/rm.DelegatingHandlers/misc/MathHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace rm.DelegatingHandlers
+{
+	public static class MathHelper
+	{
+		public static T Max<T>(T val1, T val2)
+		{
+			return Comparer<T>.Default.Compare(val1, val2) >= 0 ? val1 : val2;
+		}
+	}
+}
