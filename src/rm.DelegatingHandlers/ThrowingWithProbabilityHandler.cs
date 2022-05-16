86f68701bba5280eefcb8cc99524bbe5e8f4bedf
hippy <hippy2981@gmail.com>
2022-05-15 21:13:23 -0700

    dh: Add Throwing handlers

---
+++ src/rm.DelegatingHandlers/ThrowingWithProbabilityHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using rm.FeatureToggle;
+using rm.Random2;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Throws an exception with a probability.
+	/// </summary>
+	public class ThrowingWithProbabilityHandler : DelegatingHandler
+	{
+		private readonly double probabilityPercentage;
+		private readonly Exception exception;
+
+		private readonly IProbability probability = new Probability(rng);
+		private static readonly Random rng = RandomFactory.GetThreadStaticRandom();
+
+		/// <inheritdoc cref="ThrowingWithProbabilityHandler" />
+		public ThrowingWithProbabilityHandler(
+			double probabilityPercentage,
+			Exception exception)
+		{
+			this.probabilityPercentage = probabilityPercentage;
+			// funny, no? perhaps, 20% funny? #aurora
+			this.exception = exception
+				?? throw new ArgumentNullException(nameof(exception));
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			if (probability.IsTrue(probabilityPercentage))
+			{
+				throw exception;
+			}
+
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+}
