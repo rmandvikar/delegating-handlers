3749ef67cbcdec8431b30eef89b0fb0dd2453b83
hippy <hippy2981@gmail.com>
2022-05-15 22:22:05 -0700

    dh: Add ServicePointLogging handler

---
+++ tests/rm.DelegatingHandlersTest/ServicePointLoggingHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+using Serilog;
+using Serilog.Core;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ServicePointLoggingHandlerTests
+	{
+		[Test]
+		public async Task Logs_ServicePoint_Stats()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var loggerMock = fixture.Freeze<Mock<ILogger>>();
+			loggerMock.Setup(x => x.ForContext(It.IsAny<Type>())).Returns(loggerMock.Object);
+			loggerMock.Setup(x => x.ForContext(It.IsAny<IEnumerable<ILogEventEnricher>>())).Returns(loggerMock.Object);
+			var servicePointLoggingHandler = new ServicePointLoggingHandler(loggerMock.Object);
+			servicePointLoggingHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(servicePointLoggingHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			loggerMock.Verify((x) =>
+				x.Information("ServicePoint stats"),
+				Times.Once);
+		}
+	}
+}
