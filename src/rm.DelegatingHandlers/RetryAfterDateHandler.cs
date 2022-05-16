b9c8ae7c763479aa4e91180da6963a156e9080c4
hippy <hippy2981@gmail.com>
2022-05-15 22:23:31 -0700

    dh: Add RetryAfter handlers

---
+++ src/rm.DelegatingHandlers/RetryAfterDateHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Adds retry-after (date) header to response.
+	/// <para></para>
+	/// <see href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After">retry-after</see>
+	/// </summary>
+	public class RetryAfterDateHandler : DelegatingHandler
+	{
+		private readonly DateTimeOffset date;
+
+		/// <inheritdoc cref="RetryAfterDateHandler" />
+		public RetryAfterDateHandler(DateTimeOffset date)
+		{
+			this.date = date;
+		}
+
+		protected override async Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			var response = await base.SendAsync(request, cancellationToken)
+				.ConfigureAwait(false);
+
+			var statusCode = (int)response.StatusCode;
+			if (statusCode == 503
+				|| statusCode == 429
+				|| statusCode == 301)
+			{
+				response.Headers.Add(ResponseHeaders.RetryAfter, date.ToString("r"));
+			}
+
+			return response;
+		}
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/RetryAfterDelayHandlerTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class RetryAfterDelayHandlerTests
+	{
+		[Test]
+		[TestCase(503)]
+		[TestCase(429)]
+		[TestCase(301)]
+		public async Task Adds_RetryAfter_Header_With_Seconds(int statusCode)
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var cannedResponse = new HttpResponseMessage
+			{
+				StatusCode = (HttpStatusCode)statusCode,
+			};
+			var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(cannedResponse);
+			shortCircuitingCannedResponseHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+			var delayInSeconds = 42;
+			var retryAfterDelayHandler = new RetryAfterDelayHandler(delayInSeconds);
+			retryAfterDelayHandler.InnerHandler = shortCircuitingCannedResponseHandler;
+
+			using var invoker = new HttpMessageInvoker(retryAfterDelayHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(delayInSeconds.ToString(), response.Headers.GetValues(ResponseHeaders.RetryAfter).Single());
+		}
+
+		[Test]
+		[TestCase(200)]
+		public async Task Does_Not_Add_RetryAfter_Header(int statusCode)
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var cannedResponse = new HttpResponseMessage
+			{
+				StatusCode = (HttpStatusCode)statusCode,
+			};
+			var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(cannedResponse);
+			shortCircuitingCannedResponseHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+			var delayInSeconds = 42;
+			var retryAfterDelayHandler = new RetryAfterDelayHandler(delayInSeconds);
+			retryAfterDelayHandler.InnerHandler = shortCircuitingCannedResponseHandler;
+
+			using var invoker = new HttpMessageInvoker(retryAfterDelayHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.IsFalse(response.Headers.Contains(ResponseHeaders.RetryAfter));
+		}
+	}
+}
