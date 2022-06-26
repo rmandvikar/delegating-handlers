965ebd596f45c76522966a502d334155cb25bb38
hippy <hippy2981@gmail.com>
2022-06-26 02:43:16 -0700

    dh: Add Delegate handler

---
+++ src/rm.DelegatingHandlers/DelegateHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlersTest
+{
+	/// <summary>
+	/// Runs delegates.
+	/// </summary>
+	public class DelegateHandler : DelegatingHandler
+	{
+		private readonly Func<HttpRequestMessage, CancellationToken, Task> preDelegate;
+		private readonly Func<HttpRequestMessage, HttpResponseMessage, CancellationToken, Task> postDelegate;
+
+		/// <inheritdoc cref="DelegateHandler" />
+		public DelegateHandler(
+			Func<HttpRequestMessage, CancellationToken, Task> preDelegate = null!,
+			Func<HttpRequestMessage, HttpResponseMessage, CancellationToken, Task> postDelegate = null!)
+		{
+			// note: for flexibility, don't throw if both are null
+			this.preDelegate = preDelegate!;
+			this.postDelegate = postDelegate!;
+		}
+
+		protected async override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			if (preDelegate != null)
+			{
+				await preDelegate(request, cancellationToken)
+					.ConfigureAwait(false);
+			}
+
+			var response = await base.SendAsync(request, cancellationToken);
+
+			if (postDelegate != null)
+			{
+				await postDelegate(request, response, cancellationToken)
+					.ConfigureAwait(false);
+			}
+
+			return response;
+		}
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/DelegateHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class DelegateHandlerTests
+	{
+		[Test]
+		public async Task Delegates_Pre()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var i = 0;
+			var delegateHandler = new DelegateHandler(
+				preDelegate: (request, ct) =>
+				{
+					i++;
+					return Task.CompletedTask;
+				});
+			delegateHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(delegateHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(1, i);
+		}
+
+		[Test]
+		public async Task Delegates_Post()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var i = 0;
+			var delegateHandler = new DelegateHandler(
+				postDelegate: (request, response, ct) =>
+				{
+					i++;
+					return Task.CompletedTask;
+				});
+			delegateHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(delegateHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var _ = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(1, i);
+		}
+	}
+}
