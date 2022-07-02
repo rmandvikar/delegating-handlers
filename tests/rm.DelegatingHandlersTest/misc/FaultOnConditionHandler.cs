0dce5e3abde7310338e5ad091c1816999851e9b4
hippy <hippy2981@gmail.com>
2022-07-02 11:33:56 -0700

    dh: Add FaultWindowSignaling handlers

---
+++ tests/rm.DelegatingHandlersTest/misc/FaultOnConditionHandler.cs
@@ -0,0 +1,19 @@
+﻿using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	public class FaultOnConditionHandler : DelegatingHandler
+	{
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			// set unconditionally for test, but this can be set conditionally
+#pragma warning disable CS0618 // Type or member is obsolete
+			request.Properties[typeof(FaultWindowSignalingOnConditionHandler).FullName!] = true;
+#pragma warning restore CS0618 // Type or member is obsolete
+
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+}
