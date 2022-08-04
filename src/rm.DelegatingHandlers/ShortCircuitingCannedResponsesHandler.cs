52346635e08072a10fda0f0613f87df0565a4c43
hippy <hippy2981@gmail.com>
2022-08-04 01:01:29 -0700

    dh: Add ShortCircuitingCannedResponses handler

---
+++ src/rm.DelegatingHandlers/ShortCircuitingCannedResponsesHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Short-circuits with canned http responses.
+	/// </summary>
+	/// <remarks>
+	/// Canned response should not be disposed if used with a retry handler
+	/// as it's meant for multiuse. If so, consider using <see cref="ShortCircuitingResponseHandler"/>
+	/// instead.
+	/// </remarks>
+	public class ShortCircuitingCannedResponsesHandler : DelegatingHandler
+	{
+		private readonly HttpResponseMessage[] responses;
+		private int iResponses = 0;
+
+		/// <inheritdoc cref="ShortCircuitingCannedResponsesHandler" />
+		public ShortCircuitingCannedResponsesHandler(
+			params HttpResponseMessage[] responses)
+		{
+			this.responses = responses
+				?? throw new ArgumentNullException(nameof(responses));
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			return Task.FromResult(responses[iResponses++]);
+		}
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/ShortCircuitingCannedResponsesHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ShortCircuitingCannedResponsesHandlerTests
+	{
+		[Test]
+		public async Task ShortCircuits()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var cannedResponses = new[] { fixture.Create<HttpResponseMessage>(), fixture.Create<HttpResponseMessage>() };
+			var shortCircuitingCannedResponsesHandler = new ShortCircuitingCannedResponsesHandler(cannedResponses);
+			shortCircuitingCannedResponsesHandler.InnerHandler = fixture.Create<HttpMessageHandler>();
+
+			using var invoker = new HttpMessageInvoker(shortCircuitingCannedResponsesHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response1 = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			using var response2 = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			var responses = new[] { response1, response2 };
+
+			Assert.AreEqual(cannedResponses, responses);
+		}
+	}
+}
