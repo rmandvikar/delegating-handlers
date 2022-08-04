c55794451535f64c82ed3fbd7e689f9b1ed64643
hippy <hippy2981@gmail.com>
2022-08-04 01:01:29 -0700

    dh: Add ShortCircuitingCannedActions handler

---
+++ src/rm.DelegatingHandlers/ShortCircuitingCannedActionsHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Short-circuits with canned actions.
+	/// </summary>
+	/// <remarks>
+	/// Canned action's response could be disposed as it's not meant for multiuse.
+	/// </remarks>
+	public class ShortCircuitingCannedActionsHandler : DelegatingHandler
+	{
+		private readonly Func<HttpRequestMessage, HttpResponseMessage>[] actions;
+		private int iActions = 0;
+
+		private readonly object locker = new object();
+
+		/// <inheritdoc cref="ShortCircuitingCannedActionsHandler" />
+		public ShortCircuitingCannedActionsHandler(
+			params Func<HttpRequestMessage, HttpResponseMessage>[] actions)
+		{
+			this.actions = actions
+				?? throw new ArgumentNullException(nameof(actions));
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			lock (locker)
+			{
+				if (iActions < actions.Length)
+				{
+					return Task.FromResult(actions[iActions++](request));
+				}
+				return base.SendAsync(request, cancellationToken);
+			}
+		}
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/ShortCircuitingCannedActionsHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ShortCircuitingCannedActionsHandlerTests
+	{
+		[Test]
+		public async Task ShortCircuits()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var cannedResponses = new[] { fixture.Create<HttpResponseMessage>(), fixture.Create<HttpResponseMessage>() };
+			var cannedActions = cannedResponses.Select(x => new Func<HttpRequestMessage, HttpResponseMessage>((request) => x)).ToArray();
+
+			var shortCircuitingCannedActionsHandler = new ShortCircuitingCannedActionsHandler(cannedActions);
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				shortCircuitingCannedActionsHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response1 = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			using var response2 = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			var responses = new[] { response1, response2 };
+
+			Assert.AreEqual(cannedResponses, responses);
+		}
+
+		[Test]
+		public async Task Continues_When_Exhausted()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var cannedResponses = new[] { fixture.Create<HttpResponseMessage>() };
+			var cannedActions = cannedResponses.Select(x => new Func<HttpRequestMessage, HttpResponseMessage>((request) => x)).ToArray();
+
+			var shortCircuitingCannedActionsHandler = new ShortCircuitingCannedActionsHandler(cannedActions);
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				fixture.Create<HttpMessageHandler>(), shortCircuitingCannedActionsHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+
+			using var response1 = await invoker.SendAsync(requestMessage, CancellationToken.None);
+			using var response2 = await invoker.SendAsync(requestMessage, CancellationToken.None);
+		}
+	}
+}
