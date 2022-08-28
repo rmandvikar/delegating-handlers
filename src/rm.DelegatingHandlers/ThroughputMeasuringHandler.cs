b42e1500c09aab4d1aa0bceed047ab7e2c21bcd1
hippy <hippy2981@gmail.com>
2022-08-27 22:43:45 -0700

    dh: Add ThroughputMeasuring handler

---
+++ src/rm.DelegatingHandlers/ThroughputMeasuringHandler.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using rm.Extensions;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Measures throughput for given interval.
+	/// </summary>
+	/// <remarks>
+	/// Interval range is [1, 86,400] in seconds.
+	/// </remarks>
+	public class ThroughputMeasuringHandler : DelegatingHandler
+	{
+		private readonly IThroughputMeasuringHandlerSettings throughputMeasuringHandlerSettings;
+
+		private long i = 0;
+		private readonly System.Timers.Timer timer;
+		private const int SecondsInDay = 86_400; // 24h * 60m * 60s
+
+		/// <inheritdoc cref="ThroughputMeasuringHandler" />
+		public ThroughputMeasuringHandler(
+			IThroughputMeasuringHandlerSettings throughputMeasuringHandlerSettings)
+		{
+			_ = throughputMeasuringHandlerSettings
+				?? throw new ArgumentNullException(nameof(throughputMeasuringHandlerSettings));
+			if (throughputMeasuringHandlerSettings.IntervalInSeconds <= 0
+				|| throughputMeasuringHandlerSettings.IntervalInSeconds > SecondsInDay)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(throughputMeasuringHandlerSettings.IntervalInSeconds),
+					$"0 < {throughputMeasuringHandlerSettings.IntervalInSeconds} <= {SecondsInDay}");
+			}
+			this.throughputMeasuringHandlerSettings = throughputMeasuringHandlerSettings;
+
+			timer = new System.Timers.Timer();
+			// setup
+			SetupTimer();
+		}
+
+		private void SetupTimer()
+		{
+			timer.AutoReset = false;
+			timer.Elapsed += OnElapsedEvent!;
+
+			// bin intervals
+			var nextIntervalDelayInMilliseconds =
+				CalculateNextIntervalDelay(throughputMeasuringHandlerSettings.IntervalInSeconds);
+			timer.Interval = nextIntervalDelayInMilliseconds;
+
+			timer.Start();
+		}
+
+		/// <remarks>
+		/// Bin intervals, so the events are fired at the start of bins.
+		///
+		/// So,
+		/// for  1m intervals, event is fired at minute 00, 01, 02, etc.
+		/// for  5m intervals, event is fired at minute 00, 05, 10, etc.
+		/// for 10m intervals, event is fired at minute 00, 10, 20, etc.
+		/// for 15m intervals, event is fired at minute 00, 15, 30, etc.
+		///
+		/// The interval is calculated, and fired with range (0, interval].
+		/// </remarks>
+		internal int CalculateNextIntervalDelay(int intervalInSeconds)
+		{
+			const int MillisecondsInDay = SecondsInDay * 1000;
+			var intervalInMilliseconds = intervalInSeconds * 1000;
+			var millisecondsSinceToday = (int)DateTime.Now.TimeOfDay.TotalMilliseconds;
+			if (millisecondsSinceToday > MillisecondsInDay)
+			{
+				throw new InvalidOperationException($"millisecondsSinceToday > MillisecondsInDay. millisecondsSinceToday: {millisecondsSinceToday}, MillisecondsInDay: {MillisecondsInDay}");
+			}
+			// bin(n, interval) gives current bin
+			var nextIntervalMillisecondsSinceToday =
+				millisecondsSinceToday.Bin(intervalInMilliseconds) + intervalInMilliseconds;
+			var intervalDelayInMilliseconds = nextIntervalMillisecondsSinceToday - millisecondsSinceToday;
+			return intervalDelayInMilliseconds;
+		}
+
+		private void OnElapsedEvent(object source, ElapsedEventArgs e)
+		{
+			try
+			{
+				MeasureThroughput();
+			}
+			finally
+			{
+				// recalculate due to time creep
+				var nextIntervalDelayInMilliseconds =
+					CalculateNextIntervalDelay(throughputMeasuringHandlerSettings.IntervalInSeconds);
+				timer.Interval = nextIntervalDelayInMilliseconds;
+
+				timer.Start();
+			}
+		}
+
+		private void MeasureThroughput()
+		{
+			var throughput = unchecked((ulong)Interlocked.Exchange(ref i, 0));
+			// call even if throughput is 0
+			throughputMeasuringHandlerSettings.ThroughputProcessor.Process(throughput);
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			// note:
+			//   can overflow without exception, so the overflow check
+			//   use full range of ulong by checking for 0 instead of long.min
+			var value = unchecked((ulong)Interlocked.Increment(ref i));
+			if (value == 0)
+			{
+				throw new OverflowException($"{nameof(i)} overflow!");
+			}
+
+			return base.SendAsync(request, cancellationToken);
+		}
+
+		private bool disposed = false;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					// current interval
+					// stop timer to avoid race
+					timer?.Stop();
+					try
+					{
+						MeasureThroughput();
+					}
+					catch
+					{
+						// swallow
+					}
+
+					timer?.Dispose();
+
+					disposed = true;
+				}
+			}
+		}
+	}
+
+	public interface IThroughputMeasuringHandlerSettings
+	{
+		int IntervalInSeconds { get; }
+		IThroughputProcessor ThroughputProcessor { get; }
+	}
+
+	public interface IThroughputProcessor
+	{
+		void Process(ulong throughput);
+	}
+
+	public class ThroughputMeasuringHandlerSettings : IThroughputMeasuringHandlerSettings
+	{
+		public int IntervalInSeconds { get; init; }
+		public IThroughputProcessor ThroughputProcessor { get; init; }
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/ThroughputMeasuringHandlerTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Net;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Moq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class ThroughputMeasuringHandlerTests
+	{
+		[Explicit]
+		[Test]
+		public async Task Measures_Throughput()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			Action<ulong> processThroughput = (throughput) =>
+				Console.WriteLine($"[{DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.fff")}] throughput: {throughput,10}");
+			var throughputProcessorMock = fixture.Freeze<Mock<IThroughputProcessor>>();
+			throughputProcessorMock.Setup(x => x.Process(It.IsAny<ulong>())).Callback(processThroughput);
+			var throughputMeasuringHandler =
+				new ThroughputMeasuringHandler(
+					new ThroughputMeasuringHandlerSettings
+					{
+						IntervalInSeconds = 1,
+						ThroughputProcessor = throughputProcessorMock.Object,
+					});
+			var shortCircuitingCannedResponseHandler =
+				new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+			using var invoker = HttpMessageInvokerFactory.Create(
+				throughputMeasuringHandler, shortCircuitingCannedResponseHandler);
+
+			var durationInSeconds = 10;
+			var endTime = DateTime.Now.AddSeconds(durationInSeconds);
+			var i = (ulong)0;
+			while (DateTime.Now < endTime)
+			{
+				using var request = new HttpRequestMessage();
+				using var _ = await invoker.SendAsync(request, CancellationToken.None);
+				checked
+				{
+					i++;
+				}
+			}
+			Console.WriteLine($"[{DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.fff")}]      total: {i,10}");
+			Console.WriteLine($"[{DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.fff")}] disposing... ");
+		}
+
+		[Test]
+		[TestCase(1)]      // 1s
+		[TestCase(10)]     // 10s
+		[TestCase(60)]     // 1m
+		[TestCase(600)]    // 10m
+		[TestCase(900)]    // 15m
+		[TestCase(1_800)]  // 30m
+		[TestCase(3_600)]  // 1h
+		[TestCase(21_600)] // 6h
+		[TestCase(43_200)] // 12h
+		[TestCase(86_400)] // 1d
+		public void Verify_Interval(int intervalInSeconds)
+		{
+			var throughputMeasuringHandler = new ThroughputMeasuringHandler(
+				new ThroughputMeasuringHandlerSettings
+				{
+					IntervalInSeconds = intervalInSeconds,
+					ThroughputProcessor = null,
+				});
+			var intervalDelay = throughputMeasuringHandler.CalculateNextIntervalDelay(intervalInSeconds);
+			var now = DateTime.Now;
+			var ts = now.AddMilliseconds(intervalDelay);
+			Console.WriteLine($"now:{now:o}");
+			Console.WriteLine($" ts:{ts:o}");
+		}
+
+		[Explicit]
+		[Test]
+		public async Task Measures_Throughput_Even_When_Zero()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			Action<ulong> processThroughput = (throughput) =>
+				Console.WriteLine($"[{DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.fff")}] throughput: {throughput}");
+			var throughputProcessorMock = fixture.Freeze<Mock<IThroughputProcessor>>();
+			throughputProcessorMock.Setup(x => x.Process(It.IsAny<ulong>())).Callback(processThroughput);
+			var throughputMeasuringHandler =
+				new ThroughputMeasuringHandler(
+					new ThroughputMeasuringHandlerSettings
+					{
+						IntervalInSeconds = 1,
+						ThroughputProcessor = throughputProcessorMock.Object,
+					});
+			using var invoker = HttpMessageInvokerFactory.Create(
+				throughputMeasuringHandler);
+
+			await Task.Delay(2_000);
+			Console.WriteLine($"[{DateTime.Now.TimeOfDay.ToString(@"hh\:mm\:ss\.fff")}] disposing... ");
+		}
+	}
+}
