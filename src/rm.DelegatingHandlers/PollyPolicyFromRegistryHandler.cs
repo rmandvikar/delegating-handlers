8c7af1889b4315c8a3065ea5ff5decf3b6a65e57
hippy <hippy2981@gmail.com>
2022-10-02 19:22:26 -0700

    dh: Add Polly Policy Handlers

---
+++ src/rm.DelegatingHandlers/PollyPolicyFromRegistryHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Polly;
+using Polly.Registry;
+
+namespace rm.DelegatingHandlers
+{
+	/// <summary>
+	/// Executes the polly policy fetched from policy registry.
+	/// </summary>
+	public class PollyPolicyFromRegistryHandler : DelegatingHandler
+	{
+		private readonly IAsyncPolicy<HttpResponseMessage> policy;
+
+		/// <inheritdoc cref="PollyPolicyFromRegistryHandler" />
+		public PollyPolicyFromRegistryHandler(
+			PolicyRegistry policyRegistry,
+			IPollyPolicyFromRegistryHandlerSettings pollyPolicyFromRegistryHandlerSettings)
+		{
+			_ = policyRegistry
+				?? throw new ArgumentNullException(nameof(policyRegistry));
+			_ = pollyPolicyFromRegistryHandlerSettings
+				?? throw new ArgumentNullException(nameof(pollyPolicyFromRegistryHandlerSettings));
+
+			policy = policyRegistry.Get<IAsyncPolicy<HttpResponseMessage>>(pollyPolicyFromRegistryHandlerSettings.PolicyKey);
+		}
+
+		protected override async Task<HttpResponseMessage> SendAsync(
+			HttpRequestMessage request,
+			CancellationToken cancellationToken)
+		{
+			return await policy.ExecuteAsync((ct) => base.SendAsync(request, ct), cancellationToken)
+				.ConfigureAwait(false);
+		}
+	}
+
+	public interface IPollyPolicyFromRegistryHandlerSettings
+	{
+		string PolicyKey { get; }
+	}
+
+	public class PollyPolicyFromRegistryHandlerSettings : IPollyPolicyFromRegistryHandlerSettings
+	{
+		public string PolicyKey { get; init; }
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/PollyPolicyHandlerTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using Polly;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest
+{
+	[TestFixture]
+	public class PollyPolicyHandlerTests
+	{
+		[Test]
+		public async Task Executes_Policy_NoOp()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var noOpPolicy = Policy.NoOpAsync<HttpResponseMessage>();
+			var pollyPolicyHandler = new PollyPolicyHandler(noOpPolicy);
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				fixture.Create<HttpMessageHandler>(), pollyPolicyHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+		}
+
+		[Test]
+		public async Task Executes_Policy_Retry()
+		{
+			var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+			var retryCount = 2;
+			var retryPolicy = Policy
+				.HandleResult<HttpResponseMessage>(r => true)
+				.RetryAsync(retryCount);
+			var pollyPolicyHandler = new PollyPolicyHandler(retryPolicy);
+			var i = 0;
+			var delegateHandler = new DelegateHandler(
+				preDelegate: (request, ct) =>
+				{
+					i++;
+					return Task.CompletedTask;
+				});
+			var shortCircuitingResponseHandler = new ShortCircuitingResponseHandler(
+				new ShortCircuitingResponseHandlerSettings
+				{
+					StatusCode = (HttpStatusCode)200,
+				});
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				pollyPolicyHandler, delegateHandler, shortCircuitingResponseHandler);
+
+			using var requestMessage = fixture.Create<HttpRequestMessage>();
+			using var response = await invoker.SendAsync(requestMessage, CancellationToken.None);
+
+			Assert.AreEqual(1 + retryCount, i);
+		}
+	}
+}
