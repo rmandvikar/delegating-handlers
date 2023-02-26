58ade45a3fd7e35226ff7f86e156139988e7a5e6
hippy <hippy2981@gmail.com>
2023-02-25 23:35:37 -0800

    dh: Add http2 handler

---
+++ src/rm.DelegatingHandlers/Http2Handler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers;
+
+/// <summary>
+/// Uses http2.
+/// </summary>
+public class Http2Handler : DelegatingHandler
+{
+	/// <inheritdoc cref="Http2Handler" />
+	public Http2Handler()
+	{ }
+
+	protected override Task<HttpResponseMessage> SendAsync(
+		HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		// HttpVersion.Version20 is not available in net framework
+		request.Version = new Version("2.0");
+		return base.SendAsync(request, cancellationToken);
+	}
+}
---
+++ tests/rm.DelegatingHandlersTest/Http2HandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using rm.DelegatingHandlers;
+using rm.DelegatingHandlers.Formatting;
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.InMemory;
+using Serilog.Sinks.InMemory.Assertions;
+
+namespace rm.DelegatingHandlersTest;
+
+[TestFixture]
+public class Http2HandlerTests
+{
+#if NET6_0_OR_GREATER // test fails in net frameworks
+	[Test]
+	public async Task Sends_Http2()
+	{
+		var logger = new LoggerConfiguration()
+			.MinimumLevel.Verbose()
+			.WriteTo.InMemory()
+			.WriteTo.Console(new JsonFormatter())
+			.CreateLogger();
+		var version = "2.0";
+		var method = HttpMethod.Get;
+		var uri = "https://httpbin.org";
+		using var request =
+			new HttpRequestMessage(method, uri);
+
+		var http2Handler = new Http2Handler();
+		var loggingHandler = new LoggingHandler(logger, new LoggingFormatter());
+
+		using var httpclient = HttpClientFactory.Create(
+			http2Handler, loggingHandler);
+
+		using var _ = await httpclient.SendAsync(request, CancellationToken.None);
+
+		InMemorySink.Instance.Should()
+			.HaveMessage("request/").Appearing().Once()
+				.WithProperty($"request.Version").WithValue(version)
+			;
+		InMemorySink.Instance.Should()
+			.HaveMessage("request/response").Appearing().Once()
+				.WithProperty($"response.Version").WithValue(version)
+			;
+	}
+#endif
+}
