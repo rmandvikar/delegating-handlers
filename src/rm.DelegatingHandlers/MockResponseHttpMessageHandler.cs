6b1b1aef4fc39e79772bf7fbea7520e84f29bd59
hippy <hippy2981@gmail.com>
2023-04-08 13:32:05 -0700

    mh: Add mock http message handler

---
+++ src/rm.DelegatingHandlers/MockResponseHttpMessageHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rm.DelegatingHandlers;
+
+/// <summary>
+/// Returns mocked http response.
+/// </summary>
+/// <remarks>
+/// Short-circuits with a mock http response, and will be the last handler in the chain.
+/// </remarks>
+public class MockResponseHttpMessageHandler : HttpMessageHandler
+{
+	private readonly IMockResponseFactory mockResponseFactory;
+
+	/// <inheritdoc cref="MockResponseHttpMessageHandler" />
+	public MockResponseHttpMessageHandler(IMockResponseFactory mockResponseFactory)
+	{
+		this.mockResponseFactory = mockResponseFactory
+			?? throw new ArgumentNullException(nameof(mockResponseFactory));
+	}
+
+	protected override async Task<HttpResponseMessage> SendAsync(
+		HttpRequestMessage request,
+		CancellationToken cancellationToken)
+	{
+		return await mockResponseFactory.GetMockResponse(request, cancellationToken)
+			.ConfigureAwait(false);
+	}
+}
+
+public interface IMockResponseFactory
+{
+	Task<HttpResponseMessage> GetMockResponse(
+		HttpRequestMessage request,
+		CancellationToken cancellationToken);
+}
