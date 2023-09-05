cdfc458f03e791478b80d701574aa1eba46af78e
hippy <hippy2981@gmail.com>
2023-09-04 20:13:55 -0700

    dh: Teach to handle multiple correlationIds

--- src/rm.DelegatingHandlers/CorrelationIdHandler.cs
+++ src/rm.DelegatingHandlers/CorrelationIdHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
 using rm.Extensions;
 
 namespace rm.DelegatingHandlers;
@@ -31,23 +33,35 @@ public class CorrelationIdHandler : DelegatingHandler
 			throw new InvalidOperationException($"{RequestHeaders.CorrelationId} header already present with value '{value}'");
 		}
 
-		var correlationId = correlationIdContext.GetValue();
-		if (correlationId.IsNullOrWhiteSpace())
+		var correlationIds = correlationIdContext.GetValue();
+
+		if (correlationIds.Any(c => c.IsNullOrWhiteSpace()))
+		{
+			throw new InvalidOperationException($"{RequestHeaders.CorrelationId} header values cannot contain null/empty/whitespace value");
+		}
+		if (correlationIds.IsNullOrEmpty())
 		{
 			throw new InvalidOperationException($"{RequestHeaders.CorrelationId} header value cannot be null/empty/whitespace");
 		}
 
-		request.Headers.Add(RequestHeaders.CorrelationId, correlationId);
+		foreach (var correlationId in correlationIds)
+		{
+			request.Headers.Add(RequestHeaders.CorrelationId, correlationId);
+		}
 
 		var response = await base.SendAsync(request, cancellationToken)
 			.ConfigureAwait(false);
 
-		// add correlationId to response header
-		// if correlationId already present, noop
-		// assume valid correlationId value if already present
-		if (!response.Headers.Contains(ResponseHeaders.CorrelationId))
+		// add correlationId(s) to response header
+		// if correlationId(s) already present, dedupe but can't guarantee order
+		response.Headers.TryGetValues(ResponseHeaders.CorrelationId, out var responseCorrelationIds);
+		responseCorrelationIds = responseCorrelationIds.EmptyIfDefault();
+		foreach (var correlationId in correlationIds)
 		{
-			response.Headers.Add(ResponseHeaders.CorrelationId, correlationId);
+			if (!responseCorrelationIds.Any(responseCorrelationId => responseCorrelationId == correlationId))
+			{
+				response.Headers.Add(ResponseHeaders.CorrelationId, correlationId);
+			}
 		}
 
 		return response;
@@ -62,5 +76,5 @@ public interface ICorrelationIdContext
 	/// <summary>
 	/// Returns correlationId value.
 	/// </summary>
-	string GetValue();
+	StringValues GetValue();
 }
--- tests/rm.DelegatingHandlersTest/CorrelationIdHandlerTests.cs
+++ tests/rm.DelegatingHandlersTest/CorrelationIdHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using AutoFixture;
 using AutoFixture.AutoMoq;
+using Microsoft.Extensions.Primitives;
 using Moq;
 using NUnit.Framework;
 using rm.DelegatingHandlers;
@@ -32,7 +33,105 @@ public class CorrelationIdHandlerTests
 	}
 
 	[Test]
-	public async Task Does_Not_Add_CorrelationId_To_Response_If_Already_Present()
+	public async Task Adds_Multiple_CorrelationIds()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+		var correlationId1 = "boom1!";
+		var correlationId2 = "boom2!";
+		var correlationIdContextMock = fixture.Freeze<Mock<ICorrelationIdContext>>();
+		correlationIdContextMock.Setup(x => x.GetValue()).Returns(new StringValues(new[] { correlationId1, correlationId2, }));
+		var correlationIdHandler = fixture.Create<CorrelationIdHandler>();
+		using var invoker = HttpMessageInvokerFactory.Create(
+			correlationIdHandler, shortCircuitingCannedResponseHandler);
+		using var request = new HttpRequestMessage();
+
+		using var response = await invoker.SendAsync(request, CancellationToken.None);
+
+		Assert.IsTrue(request.Headers.Contains(RequestHeaders.CorrelationId));
+		Assert.AreEqual(new[] { correlationId1, correlationId2 }, request.Headers.GetValues(RequestHeaders.CorrelationId));
+
+		Assert.IsTrue(response.Headers.Contains(RequestHeaders.CorrelationId));
+		Assert.AreEqual(new[] { correlationId1, correlationId2 }, response.Headers.GetValues(RequestHeaders.CorrelationId));
+	}
+
+	[Test]
+	public void Throws_If_CorrelationId_Values_Is_Null_Value()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+		var correlationIdContextMock = fixture.Freeze<Mock<ICorrelationIdContext>>();
+		correlationIdContextMock.Setup(x => x.GetValue()).Returns(new StringValues(new string[] { null!, }));
+		var correlationIdHandler = fixture.Create<CorrelationIdHandler>();
+		using var invoker = HttpMessageInvokerFactory.Create(
+			correlationIdHandler, shortCircuitingCannedResponseHandler);
+		using var request = new HttpRequestMessage();
+
+		Assert.ThrowsAsync<InvalidOperationException>(async () =>
+		{
+			using var response = await invoker.SendAsync(request, CancellationToken.None);
+		});
+	}
+
+	[Test]
+	public void Throws_If_CorrelationId_Values_Is_Empty_Value()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+		var correlationIdContextMock = fixture.Freeze<Mock<ICorrelationIdContext>>();
+		correlationIdContextMock.Setup(x => x.GetValue()).Returns(new StringValues(new string[] { }));
+		var correlationIdHandler = fixture.Create<CorrelationIdHandler>();
+		using var invoker = HttpMessageInvokerFactory.Create(
+			correlationIdHandler, shortCircuitingCannedResponseHandler);
+		using var request = new HttpRequestMessage();
+
+		Assert.ThrowsAsync<InvalidOperationException>(async () =>
+		{
+			using var response = await invoker.SendAsync(request, CancellationToken.None);
+		});
+	}
+
+	[Test]
+	public void Throws_If_CorrelationId_Values_Is_WhiteSpace_Value()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+		var correlationIdContextMock = fixture.Freeze<Mock<ICorrelationIdContext>>();
+		correlationIdContextMock.Setup(x => x.GetValue()).Returns(new StringValues(new string[] { "" }));
+		var correlationIdHandler = fixture.Create<CorrelationIdHandler>();
+		using var invoker = HttpMessageInvokerFactory.Create(
+			correlationIdHandler, shortCircuitingCannedResponseHandler);
+		using var request = new HttpRequestMessage();
+
+		Assert.ThrowsAsync<InvalidOperationException>(async () =>
+		{
+			using var response = await invoker.SendAsync(request, CancellationToken.None);
+		});
+	}
+
+	[Test]
+	public async Task Does_Not_Add_CorrelationId_To_Response_If_Same_Value_Already_Present()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+		var correlationIdAlreadyPresent = "boom!";
+		var addsCorrelationIdToResponseHandler = new AddsCorrelationIdToResponseHandler(correlationIdAlreadyPresent);
+		var correlationId = "boom!";
+		var correlationIdContextMock = fixture.Freeze<Mock<ICorrelationIdContext>>();
+		correlationIdContextMock.Setup(x => x.GetValue()).Returns(correlationId);
+		var correlationIdHandler = fixture.Create<CorrelationIdHandler>();
+		using var invoker = HttpMessageInvokerFactory.Create(
+			correlationIdHandler, addsCorrelationIdToResponseHandler, shortCircuitingCannedResponseHandler);
+		using var request = new HttpRequestMessage();
+
+		using var response = await invoker.SendAsync(request, CancellationToken.None);
+
+		Assert.IsTrue(response.Headers.Contains(RequestHeaders.CorrelationId));
+		Assert.AreEqual(correlationIdAlreadyPresent, response.Headers.GetValues(RequestHeaders.CorrelationId).Single());
+	}
+
+	[Test]
+	public async Task Adds_CorrelationId_To_Response_If_Different_Value_Present()
 	{
 		var fixture = new Fixture().Customize(new AutoMoqCustomization());
 		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
@@ -49,6 +148,29 @@ public class CorrelationIdHandlerTests
 		using var response = await invoker.SendAsync(request, CancellationToken.None);
 
 		Assert.IsTrue(response.Headers.Contains(RequestHeaders.CorrelationId));
-		Assert.AreEqual(correlationIdAlreadyPresent, response.Headers.GetValues(RequestHeaders.CorrelationId).Single());
+		Assert.AreEqual(new[] { correlationIdAlreadyPresent, correlationId }, response.Headers.GetValues(RequestHeaders.CorrelationId));
+	}
+
+	[Test]
+	public async Task Adds_CorrelationId_Deduped_To_Response_If_Different_Value_Present()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage(HttpStatusCode.OK));
+		var correlationIdAlreadyPresent = "boom2!";
+		var addsCorrelationIdToResponseHandler = new AddsCorrelationIdToResponseHandler(correlationIdAlreadyPresent);
+		var correlationId1 = "boom1!";
+		var correlationId2 = "boom2!";
+		var correlationIdContextMock = fixture.Freeze<Mock<ICorrelationIdContext>>();
+		correlationIdContextMock.Setup(x => x.GetValue()).Returns(new StringValues(new[] { correlationId1, correlationId2, }));
+		var correlationIdHandler = fixture.Create<CorrelationIdHandler>();
+
+		using var invoker = HttpMessageInvokerFactory.Create(
+			correlationIdHandler, addsCorrelationIdToResponseHandler, shortCircuitingCannedResponseHandler);
+		using var request = new HttpRequestMessage();
+
+		using var response = await invoker.SendAsync(request, CancellationToken.None);
+
+		Assert.IsTrue(response.Headers.Contains(RequestHeaders.CorrelationId));
+		Assert.AreEqual(new[] { correlationId2, correlationId1 }, response.Headers.GetValues(RequestHeaders.CorrelationId));
 	}
 }
