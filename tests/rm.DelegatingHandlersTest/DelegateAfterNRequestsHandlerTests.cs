da648dc70848314b6f07e8703029e6c85fa6ddd7
hippy <hippy2981@gmail.com>
2024-02-01 23:10:17 -0800

    dh: Add Delegate after N requests handler

---
+++ tests/rm.DelegatingHandlersTest/DelegateAfterNRequestsHandlerTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Net.Http;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest;
+
+[TestFixture]
+public class DelegateAfterNRequestsHandlerTests
+{
+	[Test]
+	public async Task Runs_PreDelegate()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+		var n = 5;
+		var preDelegateExecutedCount = 0;
+		var postDelegateExecutedCount = 0;
+		var delegateAfterNRequestsHandler = new DelegateAfterNRequestsHandler(
+			new DelegateAfterNRequestsHandlerSettings
+			{
+				N = n,
+				PreDelegate = (request) =>
+				{
+					preDelegateExecutedCount++;
+					return Task.CompletedTask;
+				},
+				PostDelegate = (request, response) =>
+				{
+					postDelegateExecutedCount++;
+					return Task.CompletedTask;
+				},
+			});
+
+		using var invoker = HttpMessageInvokerFactory.Create(
+			fixture.Create<HttpMessageHandler>(), delegateAfterNRequestsHandler);
+
+		for (int i = 0; i < n; i++)
+		{
+			using var _ = await invoker.SendAsync(fixture.Create<HttpRequestMessage>(), CancellationToken.None);
+		}
+		using var __ = await invoker.SendAsync(fixture.Create<HttpRequestMessage>(), CancellationToken.None);
+
+		Assert.AreEqual(1, preDelegateExecutedCount);
+		Assert.AreEqual(1, postDelegateExecutedCount);
+	}
+
+	[Test]
+	public async Task Runs_PreDelegate_After()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+		var n = 5;
+		var preDelegateExecutedCount = 0;
+		var postDelegateExecutedCount = 0;
+		var delegateAfterNRequestsHandler = new DelegateAfterNRequestsHandler(
+			new DelegateAfterNRequestsHandlerSettings
+			{
+				N = n,
+				PreDelegate = (request) =>
+				{
+					preDelegateExecutedCount++;
+					return Task.CompletedTask;
+				},
+				PostDelegate = (request, response) =>
+				{
+					postDelegateExecutedCount++;
+					return Task.CompletedTask;
+				},
+			});
+
+		using var invoker = HttpMessageInvokerFactory.Create(
+			fixture.Create<HttpMessageHandler>(), delegateAfterNRequestsHandler);
+
+		for (int i = 0; i < n + 1; i++)
+		{
+			using var _ = await invoker.SendAsync(fixture.Create<HttpRequestMessage>(), CancellationToken.None);
+		}
+		using var __ = await invoker.SendAsync(fixture.Create<HttpRequestMessage>(), CancellationToken.None);
+
+		Assert.AreEqual(2, preDelegateExecutedCount);
+		Assert.AreEqual(2, postDelegateExecutedCount);
+	}
+
+	[Test]
+	public async Task Does_Not_Run_PreDelegate()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+		var n = 5;
+		var preDelegateExecutedCount = 0;
+		var postDelegateExecutedCount = 0;
+		var delegateAfterNRequestsHandler = new DelegateAfterNRequestsHandler(
+			new DelegateAfterNRequestsHandlerSettings
+			{
+				N = n,
+				PreDelegate = (request) =>
+				{
+					preDelegateExecutedCount++;
+					return Task.CompletedTask;
+				},
+				PostDelegate = (request, response) =>
+				{
+					postDelegateExecutedCount++;
+					return Task.CompletedTask;
+				},
+			});
+
+		using var invoker = HttpMessageInvokerFactory.Create(
+			fixture.Create<HttpMessageHandler>(), delegateAfterNRequestsHandler);
+
+		for (int i = 0; i < n; i++)
+		{
+			using var _ = await invoker.SendAsync(fixture.Create<HttpRequestMessage>(), CancellationToken.None);
+		}
+		Assert.AreEqual(0, preDelegateExecutedCount);
+		Assert.AreEqual(0, postDelegateExecutedCount);
+	}
+}
