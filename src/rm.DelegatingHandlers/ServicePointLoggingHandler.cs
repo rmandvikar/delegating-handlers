c680d3e1d3e1c4d4036bdb2c3eae0dce46386c8b
hippy <hippy2981@gmail.com>
2024-04-16 22:49:15 -0700

    dh: Tweak ServicePointLogging handler

--- src/rm.DelegatingHandlers/ServicePointLoggingHandler.cs
+++ src/rm.DelegatingHandlers/ServicePointLoggingHandler.cs
@@ -4,8 +4,6 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Serilog;
-using Serilog.Core;
-using Serilog.Core.Enrichers;
 
 namespace rm.DelegatingHandlers;
 
@@ -36,40 +34,7 @@ public class ServicePointLoggingHandler : DelegatingHandler
 		finally
 		{
 			var servicePoint = ServicePointManager.FindServicePoint(request.RequestUri);
-
-			var props =
-				new ILogEventEnricher[]
-				{
-				// global
-				new PropertyEnricher("CheckCertificateRevocationList", ServicePointManager.CheckCertificateRevocationList),
-				new PropertyEnricher("DefaultConnectionLimit", ServicePointManager.DefaultConnectionLimit),
-				new PropertyEnricher("DefaultNonPersistentConnectionLimit", ServicePointManager.DefaultNonPersistentConnectionLimit),
-				new PropertyEnricher("DefaultPersistentConnectionLimit", ServicePointManager.DefaultPersistentConnectionLimit),
-				new PropertyEnricher("DnsRefreshTimeout", ServicePointManager.DnsRefreshTimeout),
-				new PropertyEnricher("EnableDnsRoundRobin", ServicePointManager.EnableDnsRoundRobin),
-				new PropertyEnricher("EncryptionPolicy", ServicePointManager.EncryptionPolicy),
-				new PropertyEnricher("Expect100Continue", ServicePointManager.Expect100Continue),
-				new PropertyEnricher("MaxServicePointIdleTime", ServicePointManager.MaxServicePointIdleTime),
-				new PropertyEnricher("MaxServicePoints", ServicePointManager.MaxServicePoints),
-				new PropertyEnricher("ReusePort", ServicePointManager.ReusePort),
-				new PropertyEnricher("SecurityProtocol", ServicePointManager.SecurityProtocol),
-				new PropertyEnricher("UseNagleAlgorithm", ServicePointManager.UseNagleAlgorithm),
-
-				// servicePoint
-				new PropertyEnricher("host.Key", $"{servicePoint.Address.Scheme}://{servicePoint.Address.Host}"),
-				new PropertyEnricher("host.Address", servicePoint.Address),
-				new PropertyEnricher("host.ConnectionName", servicePoint.ConnectionName),
-				new PropertyEnricher("host.ProtocolVersion", servicePoint.ProtocolVersion),
-				new PropertyEnricher("host.Expect100Continue", servicePoint.Expect100Continue),
-				new PropertyEnricher("host.UseNagleAlgorithm", servicePoint.UseNagleAlgorithm),
-				new PropertyEnricher("host.SupportsPipelining", servicePoint.SupportsPipelining),
-				new PropertyEnricher("host.ConnectionLimit", servicePoint.ConnectionLimit),
-				new PropertyEnricher("host.CurrentConnections", servicePoint.CurrentConnections),
-				new PropertyEnricher("host.ConnectionLeaseTimeout", servicePoint.ConnectionLeaseTimeout),
-				new PropertyEnricher("host.IdleSince", servicePoint.IdleSince),
-				new PropertyEnricher("host.MaxIdleTime", servicePoint.MaxIdleTime),
-				new PropertyEnricher("host.ReceiveBufferSize", servicePoint.ReceiveBufferSize),
-				};
+			var props = ServicePointHelpers.GetServicePointEnrichers(servicePoint);
 
 			logger.ForContext(props)
 				.Information("ServicePoint stats");
