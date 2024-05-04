f7f65b386c237c433f4d9b495a2af4efba0a9806
hippy <hippy2981@gmail.com>
2024-05-04 16:11:31 -0700

    nuget: Rename name to rmandvikar.

--- nuget-push.sh
+++ nuget-push.sh
@@ -13,7 +13,7 @@ fi
 
 tag="v$version"
 
-dotnet nuget push .nupkg/rm.DelegatingHandlers."$version".nupkg \
+dotnet nuget push .nupkg/rmandvikar.DelegatingHandlers."$version".nupkg \
 	-k $(< ~/dump/.nuget.apikey) \
 	-s https://api.nuget.org/v3/index.json \
 	&& git push $(git remote) "$tag"
