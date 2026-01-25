62755f2bf1e81293fcecfff85f9ee6d47b2ce352
hippy <hippy2981@gmail.com>
2026-01-25 00:08:04 -0800

    invoker: Tweak HttpMessageInvokerFactory

--- tests/rm.DelegatingHandlersTest/misc/HttpMessageInvokerFactory.cs
+++ tests/rm.DelegatingHandlersTest/misc/HttpMessageInvokerFactory.cs
@@ -1,43 +1,54 @@
 using System.Net.Http;
+
 namespace rm.DelegatingHandlersTest;
 
 public static class HttpMessageInvokerFactory
 {
+	/// <remarks>
+	/// <see href="https://github.com/aspnet/AspNetWebStack/blob/77c4a761eef1ffd2944041f903257169b933fb32/src/System.Net.Http.Formatting/HttpClientFactory.cs#L13-L27">source</see>
+	/// </remarks>
 	public static HttpMessageInvoker Create(
 		params DelegatingHandler[] handlers)
 	{
+		// it is ok for innerHandler arg to be null
 		return Create(null!, handlers);
 	}
 
+	/// <remarks>
+	/// <see href="https://github.com/aspnet/AspNetWebStack/blob/77c4a761eef1ffd2944041f903257169b933fb32/src/System.Net.Http.Formatting/HttpClientFactory.cs#L47-L90">source</see>
+	/// </remarks>
 	public static HttpMessageInvoker Create(
 		HttpMessageHandler innerHandler,
 		params DelegatingHandler[] handlers)
 	{
-		if (handlers == null)
+		if (innerHandler == null && (handlers == null || !handlers.Any()))
 		{
-			throw new ArgumentNullException(nameof(handlers));
+			throw new ArgumentNullException($"{nameof(innerHandler)}, {nameof(handlers)}", "Both innerHandler and handlers are null/empty.");
+		}
+		if (innerHandler != null && (handlers == null || !handlers.Any()))
+		{
+			return new HttpMessageInvoker(innerHandler);
 		}
 		if (handlers.Any(x => x == null))
 		{
 			throw new ArgumentNullException(nameof(handlers), "At least one of the handlers is null.");
 		}
-		if (!handlers.Any())
-		{
-			return new HttpMessageInvoker(innerHandler);
-		}
 
-		var first = handlers[0];
+		var firstHandler = handlers[0];
 
-		Array.Reverse(handlers);
-		var current = innerHandler;
-		foreach (var next in handlers)
+		var reversedHandlers = handlers.AsEnumerable().Reverse();
+		// it is ok for innerHandler arg to be null
+		var nextHandler = innerHandler!;
+		foreach (var currentHandler in reversedHandlers)
 		{
-			if (current != null)
+			// see https://github.com/microsoft/referencesource/blob/ec9fa9ae770d522a5b5f0607898044b7478574a3/System/net/System/Net/Http/DelegatingHandler.cs#L21-L38
+			if (nextHandler != null)
 			{
-				next.InnerHandler = current;
+				// do not throw if currentHandler.InnerHandler is not null, simply overwrite it
+				currentHandler.InnerHandler = nextHandler;
 			}
-			current = next;
+			nextHandler = currentHandler;
 		}
-		return new HttpMessageInvoker(first);
+		return new HttpMessageInvoker(firstHandler);
 	}
 }
---
+++ tests/rm.DelegatingHandlersTest/misc/HttpMessageInvokerFactoryTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Net.Http;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using NUnit.Framework;
+using rm.DelegatingHandlers;
+
+namespace rm.DelegatingHandlersTest;
+
+[TestFixture]
+public class HttpMessageInvokerFactoryTests
+{
+	[Test]
+	public async Task Verify_Create_DelegatingHandler()
+	{
+		using var invoker = HttpMessageInvokerFactory.Create(new RelayHandler());
+	}
+
+	[Test]
+	public async Task Verify_Create_DelegatingHandlers()
+	{
+		using var invoker = HttpMessageInvokerFactory.Create(new RelayHandler(), new RelayHandler());
+	}
+
+	[Test]
+	public async Task Verify_Create_HttpMessageHandler()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+		using var invoker = HttpMessageInvokerFactory.Create(fixture.Create<HttpMessageHandler>());
+	}
+
+	[Test]
+	public async Task Verify_Create_HttpMessageHandler_DelegatingHandler()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+		using var invoker = HttpMessageInvokerFactory.Create(fixture.Create<HttpMessageHandler>(), new RelayHandler());
+	}
+
+	[Test]
+	public async Task Verify_Create_HttpMessageHandler_DelegatingHandlers()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+		using var invoker = HttpMessageInvokerFactory.Create(fixture.Create<HttpMessageHandler>(), new RelayHandler(), new RelayHandler());
+	}
+
+	[Test]
+	public async Task Verify_Create_None()
+	{
+		Assert.Throws<ArgumentNullException>(() =>
+		{
+			using var invoker = HttpMessageInvokerFactory.Create();
+		});
+	}
+
+	[Test]
+	public async Task Verify_Create_None_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() =>
+		{
+			using var invoker = HttpMessageInvokerFactory.Create(handlers: null!);
+		});
+	}
+
+	[Test]
+	public async Task Verify_Create_Null_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() =>
+		{
+			using var invoker = HttpMessageInvokerFactory.Create(innerHandler: null!, handlers: null!);
+		});
+	}
+
+	[Test]
+	public async Task Verify_Create_Value_Null()
+	{
+		var fixture = new Fixture().Customize(new AutoMoqCustomization());
+		using var invoker = HttpMessageInvokerFactory.Create(fixture.Create<HttpMessageHandler>(), handlers: null!);
+	}
+
+	[Test]
+	public async Task Verify_Create_DelegatingHandler_Null()
+	{
+		Assert.Throws<ArgumentNullException>(() =>
+		{
+			using var invoker = HttpMessageInvokerFactory.Create(handlers: [null!]);
+		});
+	}
+}
