10b55a2e76866cb796e1eca8bf94fd495ab2ee5e
hippy <hippy2981@gmail.com>
2026-07-17 17:36:01 -0700

    tests: Add missing test Logs_Request_Compact

--- tests/rm.DelegatingHandlersTest/LoggingPreHandlerTests.cs
+++ tests/rm.DelegatingHandlersTest/LoggingPreHandlerTests.cs
@@ -6,6 +6,7 @@ using rm.DelegatingHandlers;
 using rm.DelegatingHandlers.Formatting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Compact;
 using Serilog.Formatting.Json;
 using Serilog.Sinks.InMemory;
 using Serilog.Sinks.InMemory.Assertions;
@@ -75,6 +76,70 @@ public class LoggingPreHandlerTests
 				.And.WithProperty($"request.Content").WithValue(requestContent)
 				.And.WithProperty($"request.Property.{property1}").WithValue(propertyValue1)
 				.And.WithProperty($"request.Property.{property2}").WithValue(propertyValue2)
+				;
+		}
+	}
+
+	public class CompactFormatter
+	{
+		[Test]
+		public async Task Logs_Request_Compact()
+		{
+			var logger = new LoggerConfiguration()
+				.MinimumLevel.Verbose()
+				.WriteTo.InMemory()
+				.WriteTo.Console(new CompactJsonFormatter())
+				.CreateLogger();
+			var version = "2.0";
+			var method = HttpMethod.Post;
+			var uri = "/health";
+			var requestContent = "{'k':'woot?!'}".Replace("'", "\"");
+			var encoding = Encoding.UTF8;
+			var mimeType = MediaTypeNames.Application.Json;
+			var header1 = "header1";
+			var headerValue1 = "headerValue1";
+			var header2 = "header02";
+			var headerValue2 = "headerValue02";
+			var property1 = "property1";
+			var propertyValue1 = "propertyValue1";
+			var property2 = "property02";
+			var propertyValue2 = "propertyValue02";
+			using var request =
+				new HttpRequestMessage(method, uri)
+				{
+					Version = new Version(version),
+					Content = new StringContent(requestContent, encoding, mimeType),
+				};
+			request.Headers.Add(header1, headerValue1);
+			request.Headers.Add(header2, headerValue2);
+#pragma warning disable CS0618 // Type or member is obsolete
+			request.Properties.Add(property1, propertyValue1);
+			request.Properties.Add(property2, propertyValue2);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+			var shortCircuitingCannedResponseHandler = new ShortCircuitingCannedResponseHandler(new HttpResponseMessage());
+			var loggingPreHandler = new LoggingPreHandler(logger, new CompactLoggingFormatter());
+
+			using var invoker = HttpMessageInvokerFactory.Create(
+				loggingPreHandler, shortCircuitingCannedResponseHandler);
+
+			using var _ = await invoker.SendAsync(request, CancellationToken.None);
+
+			InMemorySink.Instance.Should()
+				.HaveMessage("request/").Appearing().Once()
+				.WithLevel(LogEventLevel.Information)
+					.WithProperty($"rq.v").WithValue(version)
+				.And.WithProperty($"rq.m").WithValue(method.ToString())
+				.And.WithProperty($"rq.u").WithValue(uri)
+				.And.WithProperty($"rq.h.{header1}").WithValue(headerValue1)
+				.And.WithProperty($"rq.h.{header2}").WithValue(headerValue2)
+				.And.WithProperty($"rq.c.h.Content-Type").WithValue($"{mimeType}; charset={encoding.BodyName}")
+#if NET6_0_OR_GREATER
+				.And.WithProperty($"rq.c.h.Content-Length").WithValue(requestContent.Length.ToString())
+#endif
+				.And.WithProperty($"rq.c").WithValue(requestContent)
+				.And.WithProperty($"rq.p.{property1}").WithValue(propertyValue1)
+				.And.WithProperty($"rq.p.{property2}").WithValue(propertyValue2)
 				;
 		}
 	}
